9be4dd1afc4e5d8d0a358aae2b175eca6ea616c8
Kieran Brown <kieranjbrown1@gmail.com>
2019-12-06 18:03:57 +0000

    KB - cleaning up iterations in scrape.py

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -6,8 +6,8 @@ with open('trades.psv', 'w', newline='') as file:
     writer = csv.writer(file, delimiter='|')
     writer.writerow(['item traded', 'price', 'volume', 'time', 'buy/sell', 'market/limit', 'misc'])
     response = requests.get("https://api.kraken.com/0/public/AssetPairs")
-    for x in [x for x in response.json()['result']]:
-        trades = requests.get("https://api.kraken.com/0/public/Trades",params={'pair': x}).json()['result'][x]
+    for x in response.json()['result']:
+        trades = requests.get("https://api.kraken.com/0/public/Trades", params={'pair': x}).json()['result'][x]
         for y in trades:
             writer.writerow([x, *y])
 
