5b3e86055578887813b02dff935a5a7cb170f4f7
kieranbrown <kieranjbrown1@gmail.com>
2019-12-06 19:55:18 +0000

    KB - transform.py now adds trade modifications randomly with new prices and quantities ordered; need to change timestamps

--- scraping/transform.py
+++ scraping/transform.py
@@ -1,5 +1,6 @@
 import csv
 import random
+from decimal import Decimal
 
 stocks = ['AAPL', 'MSFT', 'AMZN', 'FB', 'GOOG',
           'GOOGL', 'INTC', 'CMCSA', 'CSCO', 'PEP',
@@ -23,22 +24,36 @@ stocks = ['AAPL', 'MSFT', 'AMZN', 'FB', 'GOOG',
           'AAL', 'JBHT', 'FOXA', 'HSIC', 'MYL']
 
 
+def get_price(original_price):
+    new_price = float(original_price) * 100
+    while new_price < 1:
+        new_price = new_price * 10
+    if new_price > 1000.0:
+        new_price = new_price * 0.01
+    return round(new_price, 4)
+
+
 with open('trades.psv', 'r', newline='') as input_file:
     with open('enriched_trades.psv', 'w', newline='') as output_file:
         trades = csv.reader(input_file, delimiter='|')
         output = csv.writer(output_file, delimiter='|')
-        output.writerow(next(trades, None))
+        output.writerow(["version", *next(trades, None)])
         for row in trades:
-            row.pop(0)
-            price = float(row[1]) * 100
-            while price < 1:
-                price = price * 10
-            if price > 1000.0:
-                price = price * 0.01
+            stock = random.choice(stocks)
+            stock_quantity = int(float(row[1]) * 1000)
+            price = get_price(row[2])
 
-            output.writerow([random.choice(stocks), price, int(float(row[0]) * 1000), row[2], row[3], row[4], row[5]])
+            output.writerow(
+                [1, stock, price, stock_quantity, row[3], row[4], row[5], row[6]])
             if random.random() > 0.2:
-                print("need to add more data")
                 for i in range(random.randint(1, 5)):
-                    random.random()
+                    if random.randint(0, 1) == 1:
+                        price_change = round(Decimal(random.uniform(-0.1, 0.1)), 2)
+                        price = round(price_change.fma(Decimal(price), Decimal(price)), 4)
+
+                    stock_change = round(Decimal(random.uniform(-0.1, 0.1)), 2)
+                    stock_quantity = round(stock_change.fma(stock_quantity, stock_quantity), 0)
+                    # TODO: modify timestamp for trade modifications
+                    output.writerow(
+                        [2 + i, stock, price, stock_quantity, row[3], row[4], row[5], row[6]])
 
